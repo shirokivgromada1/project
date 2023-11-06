1eaab04d7ef4b9befc343fac8a8e3dd9f22597a1
Adamovproga2003 <gutrfknc@gmail.com>
2023-11-06 17:29:51 +0200

    SSR Dynamic pages

--- components/announcements/AnnouncementID/AnnouncementCard.tsx
+++ components/announcements/AnnouncementID/AnnouncementCard.tsx
@@ -1,61 +1,64 @@
-import styles from "./AnnouncementCard.module.scss"
-import { FC, useEffect, useState } from "react"
-import Card from "../../util/Card/Card"
-import { Announcements } from "@/tina/__generated__/types"
-import client from "@/tina/__generated__/client"
-import { AsyncReturnType } from "@/pages/[filename]"
-import ScrollToTop from "react-scroll-to-top"
-import { HiArrowNarrowUp } from "react-icons/hi"
+import styles from "./AnnouncementCard.module.scss";
+import { FC, useEffect, useState } from "react";
+import Card from "../../util/Card/Card";
+import { Announcements } from "@/tina/__generated__/types";
+import client from "@/tina/__generated__/client";
+import { AsyncReturnType } from "@/pages/[filename]";
+import ScrollToTop from "react-scroll-to-top";
+import { HiArrowNarrowUp } from "react-icons/hi";
 
 export const getMoreContent = async () => {
-	const moreAnnResponse = await client.queries.announcementsConnection({
-		last: 5,
-		sort: "pubDate",
-	})
-	return moreAnnResponse.data.announcementsConnection.edges
-}
+  const moreAnnResponse = await client.queries.announcementsConnection({
+    last: 5,
+    sort: "pubDate",
+  });
+  return moreAnnResponse.data.announcementsConnection.edges;
+};
 
 export interface AnnouncementsCardProps
-	extends Omit<Announcements, "id" | "_sys" | "__typename" | "_values"> {
-	url: string
+  extends Omit<Announcements, "id" | "_sys" | "__typename" | "_values"> {
+  filename: string | string[] | undefined;
+  url: string;
 }
 
-export const AnnouncementCard: FC<AnnouncementsCardProps> = props => {
-	const [isLoading, setLoading] = useState(false)
-	const [seeMore, setSeeMore] = useState<null | AsyncReturnType<
-		typeof getMoreContent
-	>>(null)
+export const AnnouncementCard: FC<AnnouncementsCardProps> = (props) => {
+  const [isLoading, setLoading] = useState(false);
+  const [seeMore, setSeeMore] = useState<null | AsyncReturnType<
+    typeof getMoreContent
+  >>(null);
 
-	console.log("Ann props", props)
+  console.log("Ann props", props);
 
-	useEffect(() => {
-		const getContent = async () => {
-			setLoading(true)
-			const moreAnn = await getMoreContent()
-			setSeeMore(moreAnn)
-			setLoading(false)
-		}
-		getContent()
-	}, [])
+  useEffect(() => {
+    const getContent = async () => {
+      setLoading(true);
+      const moreAnn = await getMoreContent();
+      setSeeMore(
+        moreAnn?.filter((ann) => ann?.node?._sys.filename !== props.filename)
+      );
+      setLoading(false);
+    };
+    getContent();
+  }, []);
 
-	if (!props) return <>Fetching</>
+  if (!props) return <>Fetching</>;
 
-	return (
-		<>
-			<Card
-				data={props}
-				dataMore={seeMore}
-				backHref={"/announcements"}
-				className={styles.newsCard}
-				isAnnouncement={true}
-				isLoading={isLoading}
-			/>
-			<ScrollToTop
-				smooth
-				component={<HiArrowNarrowUp />}
-				color="#309C54"
-				className={styles.buttonTop}
-			/>
-		</>
-	)
-}
+  return (
+    <>
+      <Card
+        data={props}
+        dataMore={seeMore}
+        backHref={"/announcements"}
+        className={styles.newsCard}
+        isAnnouncement={true}
+        isLoading={isLoading}
+      />
+      <ScrollToTop
+        smooth
+        component={<HiArrowNarrowUp />}
+        color="#309C54"
+        className={styles.buttonTop}
+      />
+    </>
+  );
+};
